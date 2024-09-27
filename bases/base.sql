dd38e5292ef8b7764cd4f46a9392b759f62e0a37
OniAndria254 <154087569+OniAndria254@users.noreply.github.com>
2024-09-27 19:43:34 +0300

    Initial commit

---
+++ bases/base.sql
@@ -0,0 +1,59 @@
+CREATE TABLE nature_rubrique (
+   id_nature_rubrique SERIAL,
+   nom VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_nature_rubrique)
+);
+
+CREATE TABLE type_rubrique (
+   id_type_rubrique SERIAL,
+   nom VARCHAR(30) NOT NULL,
+   PRIMARY KEY(id_type_rubrique)
+);
+
+CREATE TABLE type_centre (+
+   id_type_centre VARCHAR(50),
+   nom VARCHAR(30) NOT NULL,
+   PRIMARY KEY(id_type_centre)
+);
+
+CREATE TABLE rubrique (
+   id_rubrique SERIAL,
+   nom VARCHAR(30) NOT NULL,
+   unite_oeuvre VARCHAR(20) NOT NULL,
+   prix_unitaire DECIMAL(15,2) NOT NULL,
+   id_nature_rubrique INT NOT NULL,
+   id_type_rubrique INT NOT NULL,
+   quantite DECIMAL(10,0) NOT NULL,
+   PRIMARY KEY(id_rubrique),
+   FOREIGN KEY(id_nature_rubrique) REFERENCES nature_rubrique(id_nature_rubrique),
+   FOREIGN KEY(id_type_rubrique) REFERENCES type_rubrique(id_type_rubrique)
+);
+
+CREATE TABLE centre (
+   id_centre SERIAL,
+   nom VARCHAR(30) NOT NULL,
+   id_type_centre VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_centre),
+   FOREIGN KEY(id_type_centre) REFERENCES type_centre(id_type_centre)
+);
+
+CREATE TABLE exercice (
+   id_exercice SERIAL,
+   date_debut DATE NOT NULL,
+   date_fin DATE NOT NULL,
+   PRIMARY KEY(id_exercice)
+);
+
+CREATE TABLE liaison (
+   id_liaison SERIAL,
+   taux DECIMAL(15,2),
+   id_exercice INT NOT NULL,
+   id_centre INT NOT NULL,
+   cout_fixe DECIMAL(15,2),
+   cout_variable DECIMAL(15,2),
+   id_rubrique INT NOT NULL,
+   PRIMARY KEY(id_liaison),
+   FOREIGN KEY(id_exercice) REFERENCES exercice(id_exercice),
+   FOREIGN KEY(id_centre) REFERENCES centre(id_centre),
+   FOREIGN KEY(id_rubrique) REFERENCES rubrique(id_rubrique)
+);
---
+++ formulaires/form-equipement.jsp
@@ -0,0 +1,29 @@
+<div class="col-md-6 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Insertion charges</h4>
+        <form class="forms-sample">
+          <div class="form-group">
+           
+          <div class="form-group">
+            <label for="montant">Designation : </label>
+            <select name="designation" id="" class="form-control form-control-lg">
+              <option value="">1</option>
+              <option value="">2</option>
+              <option value="">3</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="description">Quantite : </label>
+            <input type="number" class="form-control" id="quantite" placeholder="Veuillez entrer la quantite" name="quantite">
+          </div>
+          <div class="form-group">
+            <label for="date_depenses">Date : </label>
+            <input type="date" class="form-control" id="date" placeholder="Veuillez entrer la date" name="date">
+          </div>
+          <button type="submit" class="btn btn-primary mr-2">Valider</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  
